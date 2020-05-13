cb3367ad20b18d9dad33e29df723db1234649e40
shltv <58107610+shltv@users.noreply.github.com>
2020-05-13 21:06:05 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,150 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import timedelta, datetime
+from dateutil.tz import tzlocal
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = "FizMat"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///READY.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usersAVATAR.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.permanent_session_lifetime = timedelta(days=5)
+app.permanent_session_lifetime = timedelta(hours=4)
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    avatar = db.Column(db.String(100))
+    fullname = db.Column(db.String(100))
+    fullname_lower = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    about = db.Column(db.String(200))
+    hash_pass = db.Column(db.String(100))
+
+    def __init__(self, fullname, email, username, password, about="",
+                 avatar=""):
+        self.avatar = avatar
+        self.fullname = fullname
+        self.fullname_lower = fullname.lower()
+        self.email = email
+        self.username = username
+        self.about = about
+        self.hash_pass = password
 
 
 @app.route("/")
 def welcome():
+    session["link"] = "http://127.0.0.1:8080/kz"
     return redirect(url_for("main"))
 
 
 @app.route("/kz", methods=["GET", "POST"])
 def main():
+    session["link"] = "http://127.0.0.1:8080/kz"
     if request.method == "GET":
-        return render_template("main.html", link="http://127.0.0.1")
+        return render_template("main.html", link=session["link"])
+    if request.method == "POST":
+        return redirect(url_for("login"))
+
+
+@app.route("/kz/login/", methods=["GET", "POST"])
+def login():
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
     else:
-        return None
+        session["link"] = "http://127.0.0.1:8080/kz"
+        if request.method == "GET":
+            return render_template("login.html", link=session["link"])
+        else:
+            if request.form.get('remember_me'):
+                session.permanent = False
+            else:
+                session.permanent = True
+            username = request.form["username"]
+            User = Users.query.filter_by(username=username).first()
+            if User:
+                password = request.form["password"]
+                if check_password_hash(User.hash_pass, password):
+                    session["fullname"] = User.fullname
+                    session["email"] = User.email
+                    session["hash_pass"] = User.hash_pass
+                    session["username"] = User.username
+                    return redirect(url_for("user", username=username))
+                else:
+                    flash("Сіз терген құпиясөз қате")
+                    return render_template("login.html", link=session["link"])
+            else:
+                flash("Сіз терген пайдаланушы аты қате")
+                return render_template("login.html", link=session["link"])
+
+
+@app.route("/kz/signup", methods=["GET", "POST"])
+def signup():
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+    else:
+        session["link"] = "http://127.0.0.1:8080/kz"
+        if request.method == "GET":
+            return render_template("signup.html", link=session["link"])
+        else:
+            if request.form.get('remember_me'):
+                session.permanent = False
+            else:
+                session.permanent = True
+            fullname = request.form["fullname"]
+            username = request.form["username"]
+            email = request.form["email"]
+            password = request.form["password"]
+            confirm = request.form["confirm_password"]
+            if Users.query.filter_by(username=username).all():
+                flash("Бұл пайдаланушы аты бос емес")
+                return render_template("signup.html", link=session["link"])
+            if password.isalpha() or password.isdigit():
+                flash("Құпиясөз сандар мен әріптерден тұруы қажет")
+                return render_template("signup.html", link=session["link"])
+            if len(password) < 8:
+                flash("Құпиясөз ұзындығы 8 символдан артық болуы қажет")
+                return render_template("signup.html", link=session["link"])
+            if password != confirm:
+                flash("Құпиясөзді дұрыс растамадыңыз")
+                return render_template("signup.html", link=session["link"])
+            session["username"] = username
+            session["fullname"] = fullname
+            session["email"] = email
+            password = generate_password_hash(request.form["password"])
+            session["hash_pass"] = password
+
+            User = Users(fullname=fullname, email=email, username=username, password=password)
+            db.session.add(User)
+            db.session.commit()
+            return redirect(url_for("user", username=username))
+
+
+@app.route("/kz/user/<username>", methods=["GET", "POST"])
+def user(username):
+    session["link"] = "http://127.0.0.1:8080/kz"
+    if "username" in session:
+        if request.method == "GET":
+            User = Users.query.filter_by(username=session["username"]).first()
+            return render_template("user.html", link=session["link"], User=User)
+    else:
+        return redirect(url_for("welcome"))
+
+
+@app.route("/kz/user/<username>/settings", methods=["GET", "POST"])
+def settings(username):
+    session["link"]="http://127.0.0.1:8080/kz"
+    if "username" not in session:
+        return redirect(url_for("welcome"))
+    else:
+        if request.method == "GET":
+            return render_template("settings.html", User=Users.query.filter_by(username=session["username"]).first())
+        else:
+            return redirect(url_for("user"))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(host="127.0.0.1", port=8080, debug=True)
